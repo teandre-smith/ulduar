024b3daae1b5211db4829d26bb10d342c67a118c
Te'Andre Smith <teandre.smith@tensorenergy.jp>
2022-07-26 10:23:49 +0900

    added function to return select data formatted as json

--- select.go
+++ select.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/aws/aws-sdk-go-v2/service/rdsdata"
+	"github.com/aws/aws-sdk-go-v2/service/rdsdata/types"
 )
 
 /*
@@ -21,6 +22,32 @@ func (da *DataApi) QueryRecord(s *Select) (*rdsdata.ExecuteStatementOutput, erro
 		SecretArn:   da.SecretArn,
 		Sql:         &sql,
 		Database:    da.DbName,
+	}
+
+	record, err := da.RDSClient.ExecuteStatement(context.TODO(), params)
+	if err != nil {
+		return nil, err
+	}
+
+	return record, nil
+}
+
+/*
+	Selects a record using datapi and formats as JSON.
+*/
+func (da *DataApi) QueryRecordJson(s *Select) (*rdsdata.ExecuteStatementOutput, error) {
+
+	sql, err := GenerateSelectSql(s.Options)
+	if err != nil {
+		return nil, err
+	}
+
+	params := &rdsdata.ExecuteStatementInput{
+		ResourceArn:     da.ResourceArn,
+		SecretArn:       da.SecretArn,
+		Sql:             &sql,
+		Database:        da.DbName,
+		FormatRecordsAs: types.RecordsFormatTypeJson,
 	}
 
 	record, err := da.RDSClient.ExecuteStatement(context.TODO(), params)
--- select_test.go
+++ select_test.go
@@ -46,3 +46,42 @@ func TestQueryRecord(t *testing.T) {
 	assert.Equal(expectedResult, output)
 	assert.NoError(err)
 }
+
+func TestQueryRecordJson(t *testing.T) {
+	assert := assert.New(t)
+
+	mockClient := &MockClient{}
+
+	api := DataApi{
+		RDSClient:   mockClient,
+		ResourceArn: new(string),
+		SecretArn:   new(string),
+		DbName:      new(string),
+	}
+
+	testItem := TestStruct{
+		SomeString: "",
+		SomeInt:    0,
+		SomeTime:   time.Time{},
+		SomeFloat:  0,
+	}
+
+	param := &Select{
+		Options: &SelectOptions{
+			Collection:     testItem,
+			SkipColumnList: []string{},
+			TableName:      new(string),
+			SortByColumn:   new(string),
+			SortDirection:  new(string),
+			Filter:         new(string),
+			Limit:          new(int),
+		},
+	}
+
+	output, err := api.QueryRecordJson(param)
+
+	expectedResult := &rdsdata.ExecuteStatementOutput{}
+
+	assert.Equal(expectedResult, output)
+	assert.NoError(err)
+}
